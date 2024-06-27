8af7c848812503d05a8e64748a29e72189fca0d8
pgold2 <144755945+pgold2@users.noreply.github.com>
2024-06-27 12:27:39 -0400

    Update InternChallenge.py

--- InternChallenge.py
+++ InternChallenge.py
@@ -1,3 +1,14 @@
+
 #Imports
 from pickle import GET
-
+from qdrant_client.models import Distance, VectorParams, PointStruct 
+import xml.etree.ElementTree as ET 
+import re 
+import json 
+import hashlib
+import os 
+from typing import List
+import requests 
+from qdrant_client import QdrantClient, models
+import pandas as pd 
+from urllib.parse import urlparse
